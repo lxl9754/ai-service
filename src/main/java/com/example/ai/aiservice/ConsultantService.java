6fa9b9f09ef3cd3520f3247af942de92e891bdc3
Xianglong Li <23120218@bjtu.edu.cn>
2025-09-01 14:56:47 +0800

    ai助手-流式输出，会话隔离，会话记忆与持久化，知识库检索

---
+++ src/main/java/com/example/ai/aiservice/ConsultantService.java
@@ -0,0 +1,22 @@
+package com.example.ai.aiservice;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+import reactor.core.publisher.Flux;
+
+@AiService(
+        wiringMode = AiServiceWiringMode.EXPLICIT,//手动装配
+        chatModel = "openAiChatModel",//指定模型
+        streamingChatModel = "openAiStreamingChatModel",
+        //chatMemory = "chatMemory",//配置会话记忆对象
+        chatMemoryProvider = "chatMemoryProvider",//配置会话记忆提供者对象
+        contentRetriever = "contentRetriever"//配置向量数据库检索对象
+)
+public interface ConsultantService {
+    @SystemMessage(fromResource = "system.txt")
+    public Flux<String> chat(@MemoryId String memoryId, @UserMessage String message);
+}
